10bff2fbf0f8624897a86b1713f4ddd39b2f6d83
Blake <bpro0821@gmail.com>
2019-08-23 10:54:01 -0500

    Build private route and login

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,19 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/bubblePage" component={BubblePage} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
